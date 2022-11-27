f077584b7be15cd83787280ee21e92bdd5498c2a
Michael Baudler <me@mrbandler.dev>
2022-11-27 16:10:53 +0100

    chore: Added missing background color to read-only directory and fixed config function on pwsh

--- profile.ps1
+++ profile.ps1
@@ -8,7 +8,20 @@ Set-Alias find where.exe
 
 # Functions.
 function config {
-    Start-Process -NoNewWindow -FilePath $(find git) -ArgumentList "--git-dir=$HOME\.dotfiles\ --work-tree=$HOME\ $($args -join " ")" -Wait
+    $params = @("--git-dir=$HOME\.dotfiles", "--work-tree=$HOME")
+    $params += $args
+
+    for ($i = 0; $i -lt $params.count; $i++)
+    {
+        $param = $params[$i]
+        if (($param -replace "[^\s]").length -gt 0) { $params[$i] = "`"$param`"" }
+    }
+
+    Start-Process -NoNewWindow -FilePath $(where.exe git) -ArgumentList $params -Wait
+}
+
+function cheat {
+    Invoke-RestMethod cheat.sh/$($args -join " ")
 }
 
 # Import modules.
