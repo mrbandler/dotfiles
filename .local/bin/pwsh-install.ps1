72fbddfb819bd401bdabac390f06b1367cc55895
mrbandler <me@mrbandler.dev>
2023-02-25 01:03:42 +0100

    chore(pwsh): Added install script for all used powershell modules

---
+++ .local/bin/pwsh-install.ps1
@@ -0,0 +1,3 @@
+PowerShellGet\Install-Module posh-git -Scope CurrentUser -Force
+Install-Module -Name Terminal-Icons -Repository PSGallery -Scope CurrentUser
+Install-Module PSReadLine -Force -Scope CurrentUser
