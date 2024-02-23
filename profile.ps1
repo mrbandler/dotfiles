ad06cc302c37be83c6a5738d540b7441814e626a
Michael Baudler <michael.baudler@fivefingergames.com>
2024-02-23 12:53:57 +0100

    chore(shell): Removed cheat.sh, added winfetch

--- profile.ps1
+++ profile.ps1
@@ -1,6 +1,8 @@
 # Variables.
 $env:PATH += ";$env:USERPROFILE\.local\bin"
+$env:PATH += ";$env:USERPROFILE\dagger"
 $env:STARSHIP_CONFIG = "$HOME\.config\starship.toml"
+$env:KOMOREBI_CONFIG_HOME = "$HOME\.config\komorebi"
 
 # Aliases.
 Set-Alias ll ls
@@ -11,17 +13,12 @@ function config {
     $params = @("--git-dir=$HOME\.dotfiles", "--work-tree=$HOME")
     $params += $args
 
-    for ($i = 0; $i -lt $params.count; $i++)
-    {
+    for ($i = 0; $i -lt $params.count; $i++) {
         $param = $params[$i]
         if (($param -replace "[^\s]").length -gt 0) { $params[$i] = "`"$param`"" }
     }
 
     Start-Process -NoNewWindow -FilePath $(where.exe git) -ArgumentList $params -Wait
-}
-
-function cheat {
-    Invoke-RestMethod cheat.sh/$($args -join " ")
 }
 
 # Import modules.
