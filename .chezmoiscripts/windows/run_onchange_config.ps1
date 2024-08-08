e4954357088d0d81c46ba8333f031c196c396f7a
mrbandler <me@mrbandler.dev>
2024-08-08 12:17:37 +0200

    chore(chezmoi): Tests file hashing for onchange scripts

--- .chezmoiscripts/windows/run_onchange_config.ps1
+++ .chezmoiscripts/windows/run_onchange_config.ps1
@@ -1,4 +1,4 @@
-# iac/dsc hash: {{ include "iac/dsc/root.ps1" "iac/dsc/test.txt" | sha256sum }}
+# iac/dsc hash: {{ include "iac/dsc/root.ps1" | include "iac/dsc/test.txt" | sha256sum }}
 
 echo "Applying Windows configuration..."
 
