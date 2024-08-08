7669ae2677f03f9aff075693f499d3855d8d655f
mrbandler <me@mrbandler.dev>
2024-08-08 12:10:05 +0200

    chore(chezmoi): Tests file hashing for onchange scripts

--- .chezmoiscripts/windows/run_onchange_config.ps1
+++ .chezmoiscripts/windows/run_onchange_config.ps1
@@ -1,6 +1,6 @@
 # iac/dsc hash: {{ include "iac/dsc/root.ps1" "iac/dsc/test.txt" | sha256sum }}
 
-echo "Changing Windows configuration..."
+echo "Applying Windows configuration..."
 
 # 1. Check for admin rights, if not elevate
 # 2. Apply DSC configuration
