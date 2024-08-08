1e07793daa980d0ed77cbfc081be84125e3e3291
mrbandler <me@mrbandler.dev>
2024-08-08 12:06:27 +0200

    chore(dsc): Adds general layout and plan for Windows based declarative configuration using DSC

--- .chezmoiscripts/windows/run_once_bootstrap.ps1
+++ .chezmoiscripts/windows/run_once_bootstrap.ps1
@@ -1,3 +1,8 @@
 # This script is meant to be run once to bootstrap the Windows environment.
 
 echo "Bootstrapping Windows environment..."
+
+# 1. Check for admin rights, if not elevate
+# 2. Install WinRM (this is needed to apply DSC configurations) -> winrm quickconfig
+# 3. Install package managers (scoop, choco, winget)
+# 4. Apply DSC configuration
--- .chezmoiscripts/windows/run_onchange_config.ps1
+++ .chezmoiscripts/windows/run_onchange_config.ps1
@@ -1,3 +1,6 @@
 # This script is meant to be run every time the Windows environment changes.
 
 echo "Changing Windows configuration..."
+
+# 1. Check for admin rights, if not elevate
+# 2. Apply DSC configuration
