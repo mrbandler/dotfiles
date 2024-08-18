1060ebf52599ea6207824bf610dc92e56fe44e98
mrbandler <me@mrbandler.dev>
2024-08-18 23:55:25 +0200

    chore(dsc): Removes test before configuration application

--- state/dsc/apply.ps1
+++ state/dsc/apply.ps1
@@ -2,5 +2,4 @@ $configPath = "$PSScriptRoot/configuration.dsc.yml"
 $modulesPath = "$PSScriptRoot/modules"
 $env:PSModulePath = "$env:PSModulePath;$modulesPath"
 
-winget configure test -f $configPath --verbose --disable-interactivity --accept-configuration-agreements
-winget configure -f $configPath --verbose
+winget configure -f $configPath --verbose --disable-interactivity --accept-configuration-agreements
