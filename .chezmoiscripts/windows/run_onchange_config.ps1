f5282d26d3998e0116df79b0346e60bfe8e41641
mrbandler <me@mrbandler.dev>
2024-09-22 14:53:19 +0200

    chore(scripts): Adds winget configure application to onchange for DSC configuration

--- .chezmoiscripts/windows/run_onchange_config.ps1
+++ .chezmoiscripts/windows/run_onchange_config.ps1
@@ -1,4 +1,17 @@
 echo "Applying Windows configuration..."
 
 # 1. Check for admin rights, if not elevate
+$windowsIdentity = [System.Security.Principal.WindowsIdentity]::GetCurrent()
+$windowsPrincipal = New-Object -TypeName 'System.Security.Principal.WindowsPrincipal' -ArgumentList @( $windowsIdentity )
+$isAdmin = $windowsPrincipal.IsInRole([System.Security.Principal.WindowsBuiltInRole]::Administrator)
+
+if (-not ($isAdmin)) {
+    Write-Host "This script requires administrator privileges. Relaunching with elevated rights..."
+
+    $scriptPath = $MyInvocation.MyCommand.Path
+    Start-Process powershell -ArgumentList "-ExecutionPolicy Bypass -File `"$scriptPath`"" -Verb RunAs
+    exit
+}
+
 # 2. Apply DSC configuration
+. "$PSScriptRoot\..\..\state\dsc\apply.ps1"
