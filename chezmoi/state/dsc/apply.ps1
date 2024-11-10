6aaf1ff1df54a84a5c3328c32cb17e904890cb14
mrbandler <me@mrbandler.dev>
2024-11-10 02:36:12 +0100

    fix(dsc): Re-enabled proper state apply script

--- chezmoi/state/dsc/apply.ps1
+++ chezmoi/state/dsc/apply.ps1
@@ -2,14 +2,14 @@ $configPath = "$PSScriptRoot/configuration.dsc.yml"
 $modulesPath = "$PSScriptRoot/modules"
 $env:PSModulePath = "$env:PSModulePath;$modulesPath"
 
-# $hostName = $env:COMPUTERNAME.ToLower()
-# $hostConfigPath = "$PSScriptRoot/hosts/$hostName.yml"
+$hostName = $env:COMPUTERNAME.ToLower()
+$hostConfigPath = "$PSScriptRoot/hosts/$hostName.yml"
 
-# if (-not (Test-Path $hostConfigPath)) {
-#     Write-Warning "No configuration found for host $hostName. Falling back to default configuration."
-#     $hostName = "default"
-# }
+if (-not (Test-Path $hostConfigPath)) {
+    Write-Warning "No configuration found for host $hostName. Falling back to default configuration."
+    $hostName = "default"
+}
 
-# Remove-Item -Path $configPath -Force -ErrorAction SilentlyContinue
-# ./build.ps1 -Path "$PSScriptRoot/hosts/$hostName.yml" -Output $configPath
+Remove-Item -Path $configPath -Force -ErrorAction SilentlyContinue
+./build.ps1 -Path "$PSScriptRoot/hosts/$hostName.yml" -Output $configPath
 winget configure -f $configPath --verbose --disable-interactivity --accept-configuration-agreements
