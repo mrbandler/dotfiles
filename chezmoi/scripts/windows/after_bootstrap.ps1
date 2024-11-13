9e9bf348db2506396790529f5d360b5343dac5aa
mrbandler <me@mrbandler.dev>
2024-11-13 15:35:45 +0100

    chore(win): Adds default opened apps

--- chezmoi/scripts/windows/after_bootstrap.ps1
+++ chezmoi/scripts/windows/after_bootstrap.ps1
@@ -3,16 +3,40 @@ wsl --install --no-distribution
 wsl --set-default-version 2
 wsl --install Ubuntu --no-launch
 ubuntu install --root
-wsl --set-default Ubuntu
 
 # 2. Create default user and set it for WSL
 $user = $env:USERNAME.ToLower()
-wsl -d ubuntu -u root adduser --gecos GECOS --disabled-password $user
-wsl -d ubuntu -u root usermod -aG sudo $user
+wsl -d Ubuntu -u root adduser --gecos GECOS --disabled-password $user
+wsl -d Ubuntu -u root usermod -aG sudo $user
 & ubuntu config --default-user $user
+wsl --set-default Ubuntu
 
-# 3. Unregister this scheduled task
+# 3. Start apps that need to be configured
+$startMenuPaths = @(
+    "C:\ProgramData\Microsoft\Windows\Start Menu\Programs",
+    "$env:APPDATA\Microsoft\Windows\Start Menu\Programs"
+)
+$apps = @(
+    "Flow Launcher",
+    "Voicemeeter Banana",
+    "EarTrumpet",
+    "1Password",
+    "Proton Drive",
+    "Google Drive"
+)
+
+foreach ($app in $apps) {
+    foreach ($startMenuPath in $startMenuPaths) {
+        $appPath = Get-ChildItem -Path $startMenuPath -Recurse -Filter "$app.lnk" -ErrorAction SilentlyContinue
+        if ($appPath) {
+            Start-Process -FilePath $appPath.FullName
+            break
+        }
+    }
+}
+
+# 4. Unregister this scheduled task
 Unregister-ScheduledTask -TaskName "AfterBootstrap" -Confirm:$false
 
-# 4. Finalize boostrapping
+# 5. Finalize boostrapping
 Read-Host "Finalized boostrapping. Press Enter to continue."
