7260b66d2759bbd088b1f104abb94a47dc94c27b
mrbandler <me@mrbandler.dev>
2024-11-13 13:34:42 +0100

    fix(win): After script now correctly handles autostart entries

--- chezmoi/state/dsc/after.ps1
+++ chezmoi/state/dsc/after.ps1
@@ -9,7 +9,7 @@ Import-Module "powershell-yaml"
 $content = Get-Content -Path "$PSScriptRoot/settings/autostart.yml" -Raw
 $autoStart = $content | ConvertFrom-Yaml
 
-function Is-Whitelisted {
+function Test-Whitelisted {
     param (
         [string]$EntryName
     )
@@ -33,28 +33,26 @@ $registryPaths = @(
     "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\StartupApproved\Run",
     "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\StartupApproved\Run32"
 )
-foreach ($path in $registryPaths) {
-    $entries = Get-ItemProperty -Path $path
-    echo $entries
-}
-
-foreach ($path in $registryPaths) {
-    $entries = Get-ItemProperty -Path $path
-    foreach ($entry in $entries.PSObject.Properties) {
-        if (-not (Is-Whitelisted -EntryName $entry.Name)) {
-            Remove-ItemProperty -Path $path -Name $entry.Name -ErrorAction SilentlyContinue
+foreach ($registryPath in $registryPaths) {
+    if (Test-Path -Path $registryPath) {
+        $entries = Get-ItemProperty -Path $registryPath
+        foreach ($entry in $entries.PSObject.Properties) {
+            if (-not (Test-Whitelisted -EntryName $entry.Name)) {
+                Remove-ItemProperty -Path $registryPath -Name $entry.Name -ErrorAction SilentlyContinue
+            }
         }
     }
 }
 
 $machineStartupFolderPath = "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Startup";
 $userStartupFolderPath = [System.Environment]::GetFolderPath('Startup')
-$startupFolders = @(
+$startupFolderPaths = @(
+    $machineStartupFolderPath,
     $userStartupFolderPath
 )
-foreach ($startupFolder in $startupFolders) {
-    Get-ChildItem -Path $startupFolder -Filter '*.lnk' | ForEach-Object {
-        if (-not (Is-Whitelisted -EntryName $_.BaseName)) {
+foreach ($startupFolderPath in $startupFolderPaths) {
+    Get-ChildItem -Path $startupFolderPath -Filter '*.lnk' | ForEach-Object {
+        if (-not (Test-Whitelisted -EntryName $_.BaseName)) {
             Remove-Item -Path $_.FullName -Force -ErrorAction SilentlyContinue
         }
     }
@@ -63,12 +61,13 @@ foreach ($startupFolder in $startupFolders) {
 # 3. Create or enable autostart entries
 foreach ($item in $autoStart.whitelist) {
     if ($item.type = "shortcut") {
-        $startMenuPath = "C:\ProgramData\Microsoft\Windows\Start Menu\Programs"
-        $shortcut = Get-ChildItem -Path $startMenuPath -Recurse -Filter '*.lnk' |
-            Where-Object { $_.BaseName -like $item.name } |
-            Select-Object -First 1
+        $shortcut = Get-ChildItem -Path $item.From -Recurse -Filter '*.lnk' |
+        Where-Object { $_.BaseName -like $item.name } |
+        Select-Object -First 1
 
-        $destinationPath = Join-Path -Path $startupFolderPath -ChildPath $shortcut.Name
+        if ($null -eq $shortcut) { continue }
+
+        $destinationPath = Join-Path -Path $userStartupFolderPath -ChildPath $shortcut.Name
         Copy-Item -Path $shortcut.FullName -Destination $destinationPath -Force
     }
 }
