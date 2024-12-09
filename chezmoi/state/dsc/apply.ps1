cfc15d97ead9e571d08a288495b3e77aa6932ced
Michael Baudler <me@mrbandler.dev>
2024-12-10 00:49:18 +0100

    chore(win/dsc): Adds additional output to the configuratin application process

--- chezmoi/state/dsc/apply.ps1
+++ chezmoi/state/dsc/apply.ps1
@@ -9,9 +9,15 @@ if (-not (Test-Path $hostConfigPath)) {
     Write-Warning "No configuration found for host $hostName. Falling back to default configuration."
     $hostName = "default"
 }
+else {
+    Write-Host "Using configuration for host $hostName."
+}
 
+Write-Host "Building configuration..."
 Remove-Item -Path $configPath -Force -ErrorAction SilentlyContinue
 & "$PSScriptRoot/build.ps1" -Path "$PSScriptRoot/hosts/$hostName.yml" -Output $configPath
+
+Write-Host "Applying configuration..."
 & "$PSScriptRoot/before.ps1"
 winget configure -f $configPath --verbose --disable-interactivity --accept-configuration-agreements
 & "$PSScriptRoot/after.ps1"
