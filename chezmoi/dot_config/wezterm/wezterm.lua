8e2224cb4acf849f7dfff058c76b31d1ec4d2763
Michael Baudler <me@mrbandler.dev>
2024-12-12 22:59:50 +0100

    chore(term): Adds initial WezTerm config

--- chezmoi/dot_config/wezterm/wezterm.lua
+++ chezmoi/dot_config/wezterm/wezterm.lua
@@ -1,13 +1,55 @@
--- Pull in the wezterm API
 local wezterm = require 'wezterm'
-
--- This will hold the configuration.
+local act = wezterm.action
 local config = wezterm.config_builder()
 
--- This is where you actually apply your config choices
+-- Theme & Appearance
+config.font = wezterm.font_with_fallback { 'JetBrainsMono Nerd Font', 'JetBrains Mono' }
+config.font_size = 12.0
+config.line_height = 0.9
+config.initial_rows = 60
+config.initial_cols = 238
+config.enable_scroll_bar = true
+config.color_scheme = 'Catppuccin Macchiato (Gogh)'
+config.enable_tab_bar = false
+config.window_decorations = 'RESIZE'
+config.window_padding = {
+  left = '1cell',
+  right = '1cell',
+  top = '0',
+  bottom = '1cell',
+}
+config.window_background_opacity = 0.9
+config.default_cursor_style = 'BlinkingUnderline'
+config.adjust_window_size_when_changing_font_size  = true
 
--- For example, changing the color scheme:
--- config.color_scheme = 'AdventureTime'
+-- Actions
+paste = wezterm.action_callback(function(window, pane)
+  local has_selection = window:get_selection_text_for_pane(pane) ~= ""
+  if has_selection then
+    window:perform_action(act.CopyTo("ClipboardAndPrimarySelection"), pane)
+    window:perform_action(act.ClearSelection, pane)
+  else
+    window:perform_action(act({ PasteFrom = "Clipboard" }), pane)
+  end
+end)
 
--- and finally, return the configuration to wezterm
+-- Functionality
+config.default_prog = { 'nu' }
+
+-- Keybindings
+config.leader = { key = 'Space', mods = 'CTRL|SHIFT' }
+config.keys = {
+  { key = 'V', mods = 'CTRL', action = paste },
+  { key = 'V', mods = 'CTRL', action = paste },
+}
+-- Mousbindings
+config.mouse_bindings = {
+	{
+		event = { Down = { streak = 1, button = "Right" } },
+		mods = "NONE",
+		action = paste,
+	},
+}
+
+
 return config
