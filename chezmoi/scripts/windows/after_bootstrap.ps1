0bd962e55c7a32c7d25dcbf8203fe4b0ff313aec
Michael Baudler <me@mrbandler.dev>
2025-02-02 14:16:46 +0100

    chore(bootstrap): Removes after bootstrap startup apps

--- chezmoi/scripts/windows/after_bootstrap.ps1
+++ chezmoi/scripts/windows/after_bootstrap.ps1
@@ -1,4 +1,4 @@
-# 1. Install and setup WSL
+# Install and setup WSL
 wsl --update
 wsl --install --no-distribution
 wsl --install Ubuntu --no-launch
@@ -10,33 +10,10 @@ wsl -d Ubuntu -u root usermod -aG sudo $user
 & ubuntu config --default-user $user
 wsl --set-default Ubuntu
 
-# 2. Start apps that need to be configured
-Import-Module "powershell-yaml"
-$content = Get-Content -Path "$PSScriptRoot/../../state/dsc/configuration.dsc.yml" -Raw
-$config = $content | ConvertFrom-Yaml
+# TODO: Run WSL bootstrapping here.
 
-$startMenuPaths = @(
-    "C:\ProgramData\Microsoft\Windows\Start Menu\Programs",
-    "$env:APPDATA\Microsoft\Windows\Start Menu\Programs"
-)
-foreach ($item in $config.properties.resources) {
-    if ($null -eq $item.config) { continue }
-    if ($null -eq $item.config.name) { continue }
-    if ($null -eq $item.config.bootstrap) { continue }
-    if ($null -eq $item.config.bootstrap.startAfter -or $item.bootstrap.startAfter -eq $false) { continue }
-
-    $name = $item.config.name
-    foreach ($startMenuPath in $startMenuPaths) {
-        $appPath = Get-ChildItem -Path $startMenuPath -Recurse -Filter "$name.lnk" -ErrorAction SilentlyContinue
-        if ($appPath) {
-            Start-Process -FilePath $appPath.FullName
-            break
-        }
-    }
-}
-
-# 3. Unregister this scheduled task
+# Unregister this scheduled task
 Unregister-ScheduledTask -TaskName "AfterBootstrap" -Confirm:$false
 
-# 4. Finalize boostrapping
-Read-Host "Finalized boostrapping [Enter]"
+# Finalize boostrapping
+Read-Host "Finalized boostrapping, press enter to close"
