0eaefce7a22ceef7a54e827234cadf7744567a85
= <=>
2026-02-27 23:07:43 +0100

    feat(nix): Add wpaperd configuration for zeus

---
+++ nix/homes/x86_64-linux/mrbandler@zeus/wpaperd.nix
@@ -0,0 +1,19 @@
+{ config, ... }:
+
+{
+  internal.desktop.wpaperd = {
+    enable = true;
+
+    mode = "fit";
+    monitors = {
+      DP-1 = {
+        path = "${config.home.homeDirectory}/.dotfiles/nix/wallpapers/12-5/mocha-3840x1600.png";
+        mode = "fit";
+      };
+      DP-2 = {
+        path = "${config.home.homeDirectory}/.dotfiles/nix/wallpapers/16-9/mocha-1920x1080.png";
+        mode = "fit";
+      };
+    };
+  };
+}
