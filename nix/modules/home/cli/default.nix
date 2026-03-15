62d75bcaa608d62add4f22788bb4a90f7adf72d5
mrbandler <me@mrbandler.dev>
2026-03-15 20:02:38 +0100

    refactor(nix/home): remove explicit subdirectory imports in favor of snowfall auto-import

--- nix/modules/home/cli/default.nix
+++ nix/modules/home/cli/default.nix
@@ -3,5 +3,4 @@
 }:
 
 {
-  # Subdirectories (shells/, terminals/, prompts/, tools/) are auto-imported by snowfall-lib
 }
--- nix/modules/home/desktop/default.nix
+++ nix/modules/home/desktop/default.nix
@@ -3,5 +3,4 @@
 }:
 
 {
-  # Subdirectories (core/, compositor/, shell/, wallpaper/, input/, launchers/, fileManagers/) are auto-imported by snowfall-lib
 }
