0bea18f58ea9d0014f29c959b962b26c98498212
mrbandler <me@mrbandler.dev>
2026-03-16 18:46:41 +0100

    feat(nix/cli): disable wezterm close confirmation prompt

--- nix/modules/home/cli/terminals/wezterm.nix
+++ nix/modules/home/cli/terminals/wezterm.nix
@@ -9,4 +9,8 @@ with lib;
   imports = [
     (mkAliasOptionModule [ "internal" "cli" "terminals" "wezterm" ] [ "programs" "wezterm" ])
   ];
+
+  config.programs.wezterm.extraConfig = ''
+    config.window_close_confirmation = 'NeverPrompt'
+  '';
 }
