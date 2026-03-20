d324747ad9f88bfbcac0c2f9f83ed697f4217513
mrbandler <me@mrbandler.dev>
2026-03-20 14:23:29 +0100

    feat(nix/cli): configure zellij with nushell default shell and stylix theming

--- nix/modules/home/cli/multiplexers/zellij.nix
+++ nix/modules/home/cli/multiplexers/zellij.nix
@@ -9,4 +9,12 @@ with lib;
   imports = [
     (mkAliasOptionModule [ "internal" "cli" "multiplexers" "zellij" ] [ "programs" "zellij" ])
   ];
+
+  config.programs.zellij = {
+    enable = true;
+    settings = {
+      default_shell = "nu";
+      show_startup_tips = false;
+    };
+  };
 }
