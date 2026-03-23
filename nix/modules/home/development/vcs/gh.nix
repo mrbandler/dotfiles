cf7d941c159155b941907bfd8e29a3bad63ff2b6
mrbandler <me@mrbandler.dev>
2026-03-23 13:55:45 +0100

    fix(nix/dev): remove eager op token fetch from gh module
    Let 1Password shell plugin handle gh authentication on-demand
    instead of fetching the token on every shell init.

--- nix/modules/home/development/vcs/gh.nix
+++ nix/modules/home/development/vcs/gh.nix
@@ -1,36 +1,16 @@
 {
   lib,
-  config,
   ...
 }:
 
 with lib;
-let
-  cfg = config.internal.development.vcs;
-in
 {
   imports = [
     (mkAliasOptionModule [ "internal" "development" "vcs" "gh" ] [ "programs" "gh" ])
   ];
 
-  options.internal.development.vcs.ghToken = mkOption {
-    type = types.nullOr types.str;
-    default = ''op read "op://Development/gh/token"'';
-    description = "Shell command to fetch the GH_TOKEN at shell init. Set to null to disable.";
-  };
-
-  config = {
-    programs.gh = {
-      enable = true;
-      settings.git_protocol = "ssh";
-    };
-
-    programs.bash.initExtra = mkIf (cfg.ghToken != null && config.programs.bash.enable) ''
-      export GH_TOKEN=$(${cfg.ghToken})
-    '';
-
-    programs.nushell.extraConfig = mkIf (cfg.ghToken != null && config.programs.nushell.enable) ''
-      $env.GH_TOKEN = (${cfg.ghToken})
-    '';
+  config.programs.gh = {
+    enable = true;
+    settings.git_protocol = "ssh";
   };
 }
