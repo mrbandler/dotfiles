190b57ae07c078019a98a671c6d5ae733df46df1
mrbandler <me@mrbandler.dev>
2026-04-02 21:03:04 +0200

    refactor(nix/cli): move home.packages from cli to tools module
    Also adds hcloud CLI tool.

--- nix/modules/home/cli/default.nix
+++ nix/modules/home/cli/default.nix
@@ -1,28 +1,6 @@
 {
-  pkgs,
   ...
 }:
 
 {
-  home.packages = with pkgs; [
-    jq
-    just
-    jnv
-    kmon
-    duf
-    dust
-    ncdu
-    ouch
-    process-compose
-    sd
-    tokei
-    trashy
-    gitnr
-    pdftk
-    hyperfine
-    watchexec
-    vhs
-    static-web-server
-    proton-vpn-cli
-  ];
 }
--- nix/modules/home/cli/tools/default.nix
+++ nix/modules/home/cli/tools/default.nix
@@ -1,4 +1,9 @@
 {
+  pkgs,
+  ...
+}:
+
+{
   imports = [
     ./atuin.nix
     ./bat.nix
@@ -30,5 +35,28 @@
     ./protonmail-bridge.nix
     ./khal.nix
     ./vdirsyncer.nix
+  ];
+
+  home.packages = with pkgs; [
+    jq
+    just
+    jnv
+    kmon
+    duf
+    dust
+    ncdu
+    ouch
+    process-compose
+    sd
+    tokei
+    trashy
+    gitnr
+    pdftk
+    hyperfine
+    watchexec
+    vhs
+    static-web-server
+    proton-vpn-cli
+    hcloud
   ];
 }
