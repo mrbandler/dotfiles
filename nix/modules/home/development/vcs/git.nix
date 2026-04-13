08b18e81cd182d3e50c7b2241913fd1850e7914d
mrbandler <me@mrbandler.dev>
2026-04-13 19:44:12 +0200

    feat(nix/development): add global .gitignore for OS, editor, and env files

--- nix/modules/home/development/vcs/git.nix
+++ nix/modules/home/development/vcs/git.nix
@@ -40,6 +40,64 @@ in
     programs.git = {
       enable = true;
 
+      ignores = [
+        # OS
+        ".DS_Store"
+        ".AppleDouble"
+        ".LSOverride"
+        "._*"
+        ".Spotlight-V100"
+        ".Trashes"
+        ".AppleDB"
+        ".AppleDesktop"
+        "Thumbs.db"
+        "Thumbs.db:encryptable"
+        "ehthumbs.db"
+        "ehthumbs_vista.db"
+        "[Dd]esktop.ini"
+        "$RECYCLE.BIN/"
+        "*.lnk"
+        "*~"
+        ".fuse_hidden*"
+        ".directory"
+        ".Trash-*"
+        ".nfs*"
+
+        # Editors
+        "*.swp"
+        "*.swo"
+        "*~"
+        "[._]*.s[a-v][a-z]"
+        "!*.svg"
+        "[._]*.sw[a-p]"
+        "[._]s[a-rt-v][a-z]"
+        "[._]ss[a-gi-z]"
+        "[._]*.un~"
+        "Session.vim"
+        "Sessionx.vim"
+        ".netrwhist"
+        "tags"
+        ".idea/"
+        ".vscode/*"
+        "!.vscode/settings.json"
+        "!.vscode/tasks.json"
+        "!.vscode/launch.json"
+        "!.vscode/extensions.json"
+        "!.vscode/*.code-snippets"
+        "*.code-workspace"
+
+        # Direnv
+        ".direnv"
+        ".envrc"
+
+        # Environment
+        ".env"
+        ".env.local"
+        ".env.development.local"
+        ".env.test.local"
+        ".env.production.local"
+      ];
+
       signing = mkIf vcsCfg.signing.enable {
         key = vcsCfg.signing.publicKey;
         signByDefault = true;
